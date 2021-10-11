6be2eb54848934c41bb7dd074a52ccf1345cc890
Joy <greeniejoy@gmail.com>
2021-10-11 15:20:54 -0500

    everything functiontinal except score

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -1,45 +1,78 @@
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, MouseEvent } from 'react';
 // import './App.css';
 import { questions } from '../data'
 
 function Questions() {
-  const [question, setQuestion] = useState('')
-  const [rightAnswer, setRightAnswer] = useState('')
-  const [wrongAnswer1, setWrongAnswer1] = useState('')
-  const [wrongAnswer2, setWrongAnswer2] = useState('')
-  const [wrongAnswer3, setWrongAnswer3] = useState('')
-  const [answerChoices, setAnswerChoices] = useState([])
-  const [score, setScore] = useState(0)
-  const [clicked, setClicked] = useState(false)
+  const [currQuestion, setCurrQuestion] = useState<string>('')
+  const [rightAnswer, setRightAnswer] = useState<Array<string>>([])
+  // const [wrongAnswer1, setWrongAnswer1] = useState('')
+  // const [wrongAnswer2, setWrongAnswer2] = useState('')
+  // const [wrongAnswer3, setWrongAnswer3] = useState('')
+  const [answerChoices, setAnswerChoices] = useState<Array<string>>([]);
+  const [score, setScore] = useState<number>(0)
+  const [clicked, setClicked] = useState<boolean>(false)
+  const [questionNum, setQuestionNum] = useState<number>(0)
 
-   // const [user, setQuestion] = React.useState<IQuestion | null>(null);
 
-    function shuffleArray(questions: Array<string>) {
-        for (let i = questions.length - 1; i > 0; i--) {
+
+    function shuffleArray(question: Array<string>) {
+        for (let i = question.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            [questions[i], questions[j]] = [questions[j], questions[i]];
+            [question[i], question[j]] = [question[j], question[i]];
         }
-        console.log("here is our shuffled array", questions)
-        return questions
+        return question
       }
+    
+     function nextQuestion(){
+      setClicked(true)
+      setQuestionNum(questionNum + 1)
+      if(questionNum <= questions.length - 1) {
+        console.log(questionNum)
+        setCurrQuestion(questions[questionNum].question)
+        setRightAnswer(questions[questionNum].correctAnswer)
+        setAnswerChoices(shuffleArray([questions[questionNum].answer1, questions[questionNum].answer2, questions[questionNum].answer3, questions[questionNum].answer4]))
+        console.log(currQuestion)
+      }
+      else {
+        console.log("finished!")
+       }
+    }
+
+    function updateScore(e: MouseEvent<HTMLButtonElement>){
+      console.log("we're clicking",  e.currentTarget.getAttribute('data-appMode'))
+        for(let i = 0; i < rightAnswer.length; i++){
+          console.log("right answer", rightAnswer[i])
+          if(e.currentTarget.getAttribute('data-appMode') == rightAnswer[i]){
+            setScore(score + 1)
+            console.log("our score", score)
+           }
+          else {
+             console.log("Wrong answer")
+          }
+        }
+    }
 
     useEffect(() => {
-      shuffleArray([questions[1].answer1, questions[2].answer2, questions[3].answer3, questions[4].answer4])
+  
     }, [])
 
   return (
     <div className="questions">
-      {/* {questions.map((question: any) => {
-          return (
-          <div>
-          <h3>{question.question}</h3>
-          <h3>{question.answer1}</h3>
-          <h3>{question.answer2}</h3>
-          <h3>{question.answer3}</h3>
-          <h3>{question.answer4}</h3>
-          </div>
-      )})} */}
+          {clicked === false ?
+        <h2 onClick={nextQuestion}>
+          Start
+        </h2>
+      :
+      <div>
+      <h3 >{questionNum}: {currQuestion}</h3> 
+      {answerChoices.map(answer => (
+        <button data-appMode={ answer }onClick={updateScore}>{answer}</button>
+      ))}
+      <button onClick={nextQuestion}>Next</button>
+      </div>
+    }
+    <h4>Your Score: {score}</h4>
     </div>
   );
 }
